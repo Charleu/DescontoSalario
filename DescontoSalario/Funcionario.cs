f6980f551d8b66d785f3a087da7d66f2de71800f
Charleu <sdlimeira0@gmail.com>
2024-08-29 16:49:11 -0300

    primeiro commit

---
+++ DescontoSalario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace DescontoSalario
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double CalcularSalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * porcentagem / 100.0;
+        }
+    }
+}
---
+++ DescontoSalario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DescontoSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            Console.WriteLine("Nome:");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto:");
+            func.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Imposto:");
+            func.Imposto = double.Parse(Console.ReadLine());
+            Console.WriteLine($"\nFuncionário:\nNome: {func.Nome}\nSalário Líquido: {func.CalcularSalarioLiquido():C}");
+            Console.WriteLine("\nInforme a porcentagem de aumento:");
+            func.AumentarSalario(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"\nDados do funcionário atualizados:\nNome: {func.Nome}\nSalário atualizado: {func.CalcularSalarioLiquido():C}");
+        }
+    }
+}
